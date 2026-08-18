bf76d62be07a796a6151cf4beee713a81f5bf368
Evandro Edgariano R2 <76741031+EvandroR2@users.noreply.github.com>
2026-08-17 22:00:37 -0300

    Corrige estilo e alinhamento do rodape
    Esta alteração remove o parâmetro `theme`
    não utilizado do componente estilizado `StyledFooter` no rodapé.

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -10,7 +10,7 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
 
-const StyledFooter = styled("footer")(({ theme }) => ({
+const StyledFooter = styled("footer")(() => ({
   width: "100%",
   padding: "24px 0",
   backgroundColor: "#111827",
